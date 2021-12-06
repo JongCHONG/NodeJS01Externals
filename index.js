91294bdfdbbaf8737f459d0e6b5bc53a9906d2b7
JongCHONG <yonhoa27@hotmail.fr>
2021-12-06 17:28:32 +0100

    Exercice 1

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express")
 const app = express()
-// const axios = require('axios')
+const axios = require('axios')
 const port = 5000
 const characters = require('./characters.json')
 
 //Exercice 1
-
 app.get('/game-of-thrones/json', (req, res) => {
   res.send(characters)
+})
+
+app.get('/game-of-thrones/url', (req, res) => {
+  axios.get('https://thronesapi.com/api/v2/Characters')
+    .then(response => res.json(response.data))
+    .catch(error => res.status(error.response.status).send("Not found"))
 })
 
 app.listen(port, () => {
