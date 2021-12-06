26755d3d59861159b5c6274785b849f527216b6a
unknown <jochong27@gmail.com>
2021-12-06 22:15:59 +0100

    Exercice 2

--- index.js
+++ index.js
@@ -15,6 +15,21 @@ app.get('/game-of-thrones/url', (req, res) => {
     .catch(error => res.status(error.response.status).send("Not found"))
 })
 
+//Exercice 2
+app.get('/pokemon/limit/:limit/offset/:offset', (req, res) => {
+  const { limit, offset } = req.params
+  axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`)
+    .then(response => res.json(response.data))
+    .catch(error => res.status(error.response.status).send("Not found"))
+})
+
+app.get('/pokemon/id/:id', (req, res) => {
+  const { id } = req.params
+  axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+    .then(response => res.json(response.data))
+    .catch(error => res.status(error.response.status).send("Not found"))
+})
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
